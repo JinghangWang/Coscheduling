44bf062a80e5ab4c9a63d653a67c1a6ef3470b0f
JinghangWang <jinghangwang2018@u.northwestern.edu>
2017-07-10 15:11:34 -0500

    Revert "Improve logging output to serial"
    This reverts commit 9a8e2d421305914c706d490014c970460814d61a.

--- src/dev/serial.c
+++ src/dev/serial.c
@@ -66,7 +66,7 @@
 
 
 /* This state supports early output */
-/* the lock is reused for late output to serialize lines / chars */
+
 static spinlock_t serial_lock; /* for SMP */
 static uint8_t serial_device_ready = 0;
 static uint16_t serial_io_addr = 0;
@@ -201,12 +201,6 @@ static int serial_do_write(void *state, uint8_t *src)
     spin_unlock_irq_restore(&s->output_lock, flags);
     return rc;
 }
-
-static void serial_do_write_wait(void *state, uint8_t *src)
-{
-    while (serial_do_write(state,src)==0) {}
-}
-
 
 static struct nk_char_dev_int chardevops = {
     .get_characteristics = serial_do_get_characteristics,
@@ -703,19 +697,16 @@ static void serial_putchar_early (uchar_t c)
 
 void serial_putchar(uchar_t c)
 {
-
-    if (early_dev) {
-      int flags = spin_lock_irq_save(&serial_lock);
-      if (c=='\n') { 
-         serial_do_write_wait(early_dev,"\r");
-      }
-
-      serial_do_write_wait(early_dev,&c);
-      spin_unlock_irq_restore(&serial_lock,flags);
-    } else {
-      serial_putchar_early(c);
-    }
-
+  if (0 && early_dev) { // patch
+  // if (early_dev){
+  	if (c=='\n') { 
+  	    serial_do_write(early_dev,"\r");
+  	}
+  	serial_do_write(early_dev,&c);
+  } else {
+  	serial_putchar_early(c);
+  }
+	
 }
 
 
@@ -723,25 +714,11 @@ void serial_putchar(uchar_t c)
 void 
 serial_write (const char *buf) 
 {
-    if (early_dev) {
-	char c;
-	int flags = spin_lock_irq_save(&serial_lock);
-	while ((c=*buf)) { 
-	    if (c=='\n') { 
-		serial_do_write_wait(early_dev,"\r");
-	    }
-	    serial_do_write_wait(early_dev,&c);
-	    buf++;
-	}
-	spin_unlock_irq_restore(&serial_lock,flags);
-    } else {
-	while (*buf) {
-	    serial_putchar(*buf);
-	    ++buf;
-	}
-    }
+  while (*buf) {
+      serial_putchar(*buf);
+      ++buf;
+  }
 }
-
 
 void 
 serial_puts( const char *buf)
